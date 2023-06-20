bb38f03e42a12bb0052f2034a3a392a2e98000d3
kangluyao <luyaokang@outlook.com>
2023-06-20 22:33:59 +0800

    update

--- script/edgeR analysis.R
+++ script/edgeR analysis.R
@@ -89,10 +89,109 @@ length(intersect(select_sur_genes, select_sub_genes))
 length(intersect(select_sur_genes, select_pl_genes))
 length(intersect(select_sub_genes, select_pl_genes))
 ############################################################################################################
-# 2. Volcano plot
+# 2. KEGG decoder for all genes among three layers
+kegg_decoder_fun <- function(layer, df) {
+  ko_count_table = df
+  samples <- grep(layer, meta_dat$sample_id, value = T)
+  kegg_decoder_df = data.frame(Sample = c(), KEGG = c())
+  for (kegg_id in rownames(ko_count_table)) {
+    for (sample in samples) {
+      if (ko_count_table[kegg_id, sample] > 0) {
+        kegg_decoder_df = rbind(kegg_decoder_df, data.frame(Sample = sample, KEGG = kegg_id))
+      }
+    }
+  }
+  return(kegg_decoder_df)
+}
+kegg_decoder_sur_df <- kegg_decoder_fun('SUR', ko_count_table)
+kegg_decoder_sub_df <- kegg_decoder_fun('SUB', ko_count_table)
+kegg_decoder_pl_df <- kegg_decoder_fun('PL', ko_count_table)
 
+kegg_decoder_sur_df$Sample <- sapply(kegg_decoder_sur_df$Sample, FUN = function(x){sub('_', '', x)})
+kegg_decoder_sub_df$Sample <- sapply(kegg_decoder_sub_df$Sample, FUN = function(x){sub('_', '', x)})
+kegg_decoder_pl_df$Sample <- sapply(kegg_decoder_pl_df$Sample, FUN = function(x){sub('_', '', x)})
+
+write.table(kegg_decoder_sur_df,
+            file.path(save.dir, 'tables/kegg/kegg_decoder/KEGG_decoder_sur_df_in.tsv'), sep='\t',  col.names=FALSE, row.names=FALSE , quote=FALSE)
+write.table(kegg_decoder_sub_df,
+            file.path(save.dir, 'tables/kegg/kegg_decoder/KEGG_decoder_sub_df_in.tsv'), sep='\t',  col.names=FALSE, row.names=FALSE , quote=FALSE)
+write.table(kegg_decoder_pl_df,
+            file.path(save.dir, 'tables/kegg/kegg_decoder/KEGG_decoder_pl_df_in.tsv'), sep='\t',  col.names=FALSE, row.names=FALSE , quote=FALSE)
+
+# in the Data folder using the CryoBiome-kegg-decoder env: 
+# cd /mnt/e/permafrost/result/tables/kegg/kegg_decoder
+# python /mnt/e/permafrost/script/KEGG-decoder.py -i KEGG_decoder_sur_df_in.tsv -o KEGG_decoder_sur_df_output.tsv
+# python /mnt/e/permafrost/script/KEGG-decoder.py -i KEGG_decoder_sub_df_in.tsv -o KEGG_decoder_sub_df_output.tsv
+# python /mnt/e/permafrost/script/KEGG-decoder.py -i KEGG_decoder_pl_df_in.tsv -o KEGG_decoder_pl_df_output.tsv
+# python -m pip install xxxx
+KEGG_decoder_sur <- read.table(file.path(save.dir, "tables/kegg/kegg_decoder/KEGG_decoder_sur_df_output.tsv"), 
+                               sep = "\t", header = TRUE)
+KEGG_decoder_sub <- read.table(file.path(save.dir, "tables/kegg/kegg_decoder/KEGG_decoder_sub_df_output.tsv"),
+                               sep = "\t", header = TRUE)
+KEGG_decoder_pl <- read.table(file.path(save.dir, "tables/kegg/kegg_decoder/KEGG_decoder_pl_df_output.tsv"),
+                              sep = "\t", header = TRUE)
+KEGG_decoder <- rbind(KEGG_decoder_sur, KEGG_decoder_sub, KEGG_decoder_pl)
+
+colsums <- colSums(KEGG_decoder[, !colnames(KEGG_decoder) %in% c("Function")])
+KEGG_decoder <- KEGG_decoder[, c("Function", names(colsums[colsums > 0]))]
+names(KEGG_decoder)[names(KEGG_decoder) == "Function"] <- "Sample"
+KEGG_decoder <- reshape2::melt(KEGG_decoder, variable.name = "Function", value.name = "Completion")
+
+# Renaming pathways for plotting
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "TCA.Cycle"] <- "TCA Cycle"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "NAD.P.H.quinone.oxidoreductase"] <- "NAD(P)H-quinone oxidoreductase"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "X3.Hydroxypropionate.Bicycle"] <- "3-Hydroxypropionate Bicycle"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "basic.endochitinase.B"] <- "Basic endochitinase B"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "DMS.dehydrogenase"] <- "DMS dehydrogenase"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "thiamin.biosynthesis"] <- "Thiamin biosynthesis"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "transporter..vitamin.B12"] <- "Transporter: vitamin B12"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Flagellum"] <- "Flagellum"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Photosystem.II"] <- "Photosystem II"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Photosystem.I"] <- "Photosystem I"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Cytochrome.b6.f.complex"] <- "Cytochrome b6/f complex"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Entner.Doudoroff.Pathway"] <- "Entner-Doudoroff Pathway"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Mixed.acid..Ethanol..Acetate.to.Acetylaldehyde"] <- "Mixed acid: Ethanol, Acetate to Acetylaldehyde"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Mixed.acid..Ethanol..Acetyl.CoA.to.Acetylaldehyde..reversible."] <- "Mixed acid: Ethanol, Acetyl-CoA to Acetylaldehyde (reversible)"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Naphthalene.degradation.to.salicylate"] <- "Naphthalene degradation to salicylate"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Curli.fimbriae.biosynthesis"] <- "Curli fimbriae biosynthesis"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Competence.related.core.components"] <- "Competence-related core components"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "CP.lyase.operon"] <- "CP-lyase operon"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Type.III.Secretion"] <- "Type III Secretion"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Type.IV.Secretion"] <- "Type IV Secretion"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "Type.Vabc.Secretion"] <- "Type Vabc Secretion"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "serine"] <- "Serine"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "tyrosine"] <- "Tyrosine"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "starch.glycogen.degradation"] <- "Starch/glycogen degradation"
+levels(KEGG_decoder$Function)[levels(KEGG_decoder$Function) == "end.product.astaxanthin"] <- "End-product astaxanthin"
+
+KEGG_decoder$mean_comp = vapply(1:nrow(KEGG_decoder), function(x) mean(KEGG_decoder$Completion[KEGG_decoder$Function == KEGG_decoder$Function[x]]), FUN.VALUE = numeric(1))
+KEGG_decoder$Function <- factor(KEGG_decoder$Function, levels=unique((KEGG_decoder$Function)[order(KEGG_decoder$mean_comp)]))
+meta_dat$Sample <- sapply(meta_dat$sample_id, FUN = function(x){sub('_', '', x)})
+KEGG_decoder$layer = sapply(1:nrow(KEGG_decoder), function(x) meta_dat$layer[meta_dat$Sample == KEGG_decoder$Sample[x]])
+KEGG_decoder$Sample <- factor(KEGG_decoder$Sample, levels = meta_dat$Sample, ordered = T)
+KEGG_decoder$layer <- factor(KEGG_decoder$layer, levels = c('SUR', 'SUB', 'PL'), ordered = T)
+
+# plot
+library(ggplot2)
+p <- ggplot(KEGG_decoder) + 
+  geom_tile(aes(x = as.factor(Sample), y = as.factor(Function), fill = Completion)) +
+  xlab("") + ylab("") + 
+  theme_linedraw() + 
+  guides(fill = guide_colourbar(barwidth = 0.5, barheight = 10)) +
+  scale_fill_gradientn(colours = c("white", "#00A087FF"), values = c(0, 1)) + 
+  facet_grid(~layer, scales = "free_x", space = "free_x") + 
+  theme(axis.title.x = element_blank(), panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank(), 
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 5), 
+        axis.text.y = element_text(size = 6), 
+        panel.spacing = unit(0, "lines"),
+        legend.title = element_text(size = 6),
+        legend.text = element_text(size = 6),
+        strip.text = element_blank(),
+        strip.background = element_blank())
+p
 ############################################################################################################
-# 3. KEGG decoder
+# 3. KEGG decoder for the variable genes determined by the edgeR analysis
 kegg_decoder_fun <- function(layer, kegg_sig) {
   samples <- grep(layer, meta_dat$sample_id, value = T)
   kegg_decoder_df = data.frame(Sample = c(), KEGG = c())
