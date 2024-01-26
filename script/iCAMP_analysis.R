0da55eb080cefb97bf2d3565b64cc195c98d47fb
kangluyao <luyaokang@outlook.com>
2024-01-26 10:43:10 +0800

    update

--- script/iCAMP_analysis.R
+++ script/iCAMP_analysis.R
@@ -2,23 +2,23 @@ save.dir = "E:/permafrost/result"
 if(!dir.exists(save.dir)){dir.create(save.dir)}
 setwd(save.dir)
 library(iCAMP)
-#extract the top 500 ASVs in each layer
-library(tidyverse)
-top_n_sel <- function(i, N) {
-  as.data.frame(otu[, grep(i, colnames(otu))]) %>% 
-    mutate(mean_abundance = rowMeans(.)) %>% 
-    arrange(desc(mean_abundance)) %>% top_n(500, mean_abundance) %>% 
-    select(grep(i, colnames(otu))) %>% rownames()
-}
-otu_top500_sur <- top_n_sel('SUR', 500)
-otu_top500_sub <- top_n_sel('SUB', 500)
-otu_top500_pl <- top_n_sel('PL', 500)
-otu_top500 <- full_join(otu_top500_sur, otu_top500_sub, otu_top500_pl)
-
-otu_top500_sur <- as.data.frame(otu[, grep('SUR', colnames(otu))]) %>% 
-  mutate(mean_abundance = rowMeans(.)) %>% 
-  arrange(desc(mean_abundance)) %>% top_n(500, mean_abundance) %>% 
-  select(grep('SUR', colnames(otu))) %>% rownames()
+# extract the top 500 ASVs in each layer
+# library(tidyverse)
+# top_n_sel <- function(i, N) {
+#   as.data.frame(otu[, grep(i, colnames(otu))]) %>% 
+#     mutate(mean_abundance = rowMeans(.)) %>% 
+#     arrange(desc(mean_abundance)) %>% top_n(500, mean_abundance) %>% 
+#     select(grep(i, colnames(otu))) %>% rownames()
+# }
+# otu_top500_sur <- top_n_sel('SUR', 500)
+# otu_top500_sub <- top_n_sel('SUB', 500)
+# otu_top500_pl <- top_n_sel('PL', 500)
+# otu_top500 <- full_join(otu_top500_sur, otu_top500_sub, otu_top500_pl)
+# 
+# otu_top500_sur <- as.data.frame(otu[, grep('SUR', colnames(otu))]) %>% 
+#   mutate(mean_abundance = rowMeans(.)) %>% 
+#   arrange(desc(mean_abundance)) %>% top_n(500, mean_abundance) %>% 
+#   select(grep('SUR', colnames(otu))) %>% rownames()
 
 phylo_sur <- subset_samples(phylo, layer == 'SUR')
 phylo_sur <- prune_taxa(taxa_sums(phylo_sur) >= 1, phylo_sur)
@@ -27,7 +27,7 @@ phylo_sub <- prune_taxa(taxa_sums(phylo_sub) >= 1, phylo_sub)
 phylo_pl <- subset_samples(phylo, layer == 'PL')
 phylo_pl <- prune_taxa(taxa_sums(phylo_pl) >= 1, phylo_pl)
 
-#以iCAMP包的内置数据集example.data为例(行样本, 列 OTU)，包含一个小型微生物群落数据及它们的系统发育树
+# extract the community tables and phylogenetic trees for each layer
 comm_sur <- t(otu_table(phylo_sur))
 tree_sur <- phy_tree(phylo_sur)
 
@@ -37,16 +37,14 @@ tree_sub <- phy_tree(phylo_sub)
 comm_pl <- t(otu_table(phylo_pl))
 tree_pl <- phy_tree(phylo_pl)
 
-#基于 Ning et al (2020) 的方法的群落构建分析
-#ses.cut = 1.96，以 βNRI=1.96 作为同质和异质选择的阈值；rc.cut=0.95，以RC=0.95 作为扩散和漂变的阈值
-#bin.size.limit 用来指定 bin 中的分类单元数,这个数据集太小因此使用使用 5，实际情况中可根据系统发育信号测试或尝试一些设置选择合理的 bin，作者的经验是 12、24、48 等
-#rand指定随机化次数构建零分布，nworker用来多线程运行以提升计算速率
-#更多详情 ?icamp.big
+# Community assembly analysis based on the iCAMP framwork developed by Ning et al (2020) 
+# ses.cut = 1.96, with βNRI=1.96 as the threshold for homogeneous and heterogeneous selection; 
+# rc.cut=0.95, with RC=0.95 as the threshold for diffusion and drift.
 set.seed(123)
 icamp.sur.out <- icamp.big(comm = comm_sur, tree = tree_sur, pd.wd = paste0(save.dir,"/tables/null_model/sur"), 
                        ses.cut = 1.96, rc.cut = 0.95, bin.size.limit = 24, 
                        rand = 1000, nworker = 8)
-#异质选择（Heterogeneous.Selection）、同质选择（Homogeneous.Selection）、扩散限制（Dispersal.Limitation）、同质扩散（Homogenizing.Dispersal）以及漂变（Drift.and.Others）的样本对得分
+# Heterogeneous.Selection, Homogeneous Selection, Dispersal Limitation, Homogenizing.Dispersal, Drift and Others
 head(icamp.sur.out$CbMPDiCBraya)
 write.csv(icamp.sur.out$CbMPDiCBraya, 
           file.path(save.dir, './tables/null_model/sur/iCAMP.process.CbMPDiCBraya.csv'))
@@ -84,7 +82,7 @@ null_df <- rbind(cbind(layer = rep('SUR', nrow(null_sur)), null_sur[, 3:7]),
 
 library(ggalluvial)
 
-#alluvial diagram
+# alluvial diagram
 null_plot <- null_df %>%
   group_by(layer) %>%
   summarise(across(everything(), mean)) %>%
